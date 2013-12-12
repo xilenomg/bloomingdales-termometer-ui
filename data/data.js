917ae32bc048add4550d6d90c2a75a6e1c905598
Luis Felipe Perez <xilenomg@gmail.com>
2013-12-12 12:19:07 -0500

    adding files

---
+++ data/data.js
@@ -0,0 +1,6 @@
+var data = [
+	 {first_name: "Heidi", last_name: "Ellsworth", date: "", hour:"", minute: ""},
+	 {first_name: "Chelsey", last_name: "Puttick", date: "", hour:"", minute: ""},
+	 {first_name: "Dana", last_name: "Olsen", date: "", hour:"", minute: ""},
+	 {first_name: "Samantha", last_name: "Bird", date: "", hour:"", minute: ""}
+];
