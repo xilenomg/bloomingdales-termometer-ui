535cd12db4d8ab5bee93dc3d810c782affd905e3
Luis Felipe Perez <xilenomg@gmail.com>
2013-12-12 20:17:44 -0500

    adding counter

--- js/termometer.js
+++ js/termometer.js
@@ -1,29 +1,30 @@
-var Thermometer = function(bottomImageElement, topImageElement, partialImageElement, lastImageURL){
+var Thermometer = function(currentAmountValue){
 	//constants values
 	var maxPercentageValue = 80;
-	var maxAmountValue = 500000;
-	var currentAmountValue = 460000;
-	
+	var maxAmountValue = 500000000;
+
 	//self variable referecing to this own class
 	var self = this;
-	
-	//defining variables
-	self.bottomImageElement = $(bottomImageElement);
-	self.topImageElement = $(topImageElement);
-	self.partialImageElement = $(partialImageElement);
-	self.lastImageURL = lastImageURL;
-	
+
+	self.currentAmountValue = currentAmountValue;
+
+	//defining variables, , , 'images/pink_bag.png'
+	self.bottomImageElement = $(".brown-bag");
+	self.topImageElement = $(".pink-bag");
+	self.partialImageElement = $(".pink-bag-top");
+	self.counterElement = $(".numberCounter");
+
 	//constructor
 	self.init = function(){
 		var heightValueTo = self.calculateHeightValue();
 		self.animate(heightValueTo);
 	};
-	
+
 	self.calculateHeightValue = function(){
 		//getting percentage value of current amount value from max value
-		var percentage = currentAmountValue / maxAmountValue * 100;
+		var percentage = self.currentAmountValue / maxAmountValue * 100;
 		var value = maxPercentageValue * percentage / 100;
-		
+
 		return value;
 	};
 
@@ -32,33 +33,65 @@ var Thermometer = function(bottomImageElement, topImageElement, partialImageElem
 			height: heightValue + "%"
 		}, 
 		{
-			duration: 1000,
+			duration: 700,
 			step: function( now, fx ){
 				self.handlePartialImage(now, fx);
+				self.updateCounterNumber(now, fx);
 			}
 		});
 	};
-	
+
+	self.updateCounterNumber = function(now, fx){
+		var percentage = now/fx.end  * 100;
+		var value = self.currentAmountValue * percentage / 100;
+		self.counterElement.html("$ " + self.formatNumber(value.toFixed(2)));
+	};
+
 	self.handlePartialImage = function(now, fx){
-		partialImageElement.css("top",(self.topImageElement.position().top)-21);
-		partialImageElement.css("left", self.calculateLeftGap(now, fx));
+		self.partialImageElement.css("top",self.calculateTopGap(now, fx));
+		self.partialImageElement.css("left", self.calculateLeftGap(now, fx));
 	};
 
 	var gapStarts = -4;
 	var gapEnds = 0;
 	var diff = gapEnds - gapStarts;
-	
+
+	self.calculateTopGap = function(now, fx) {
+		return self.topImageElement.position().top - 21;
+	};
+
 	self.calculateLeftGap = function(now,fx){
 		var percentage = now/maxPercentageValue  * 100;
 		var finalValue = (diff - (percentage * 4 / 100)) * -1;
 		return finalValue;
 	};
+
+	self.formatNumber = function (number) {
+		var decimals = 2;
+		dec_point = ".";
+		thousands_sep = ",";
+		var n = !isFinite(+number) ? 0 : +number, 
+		        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+		        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+		        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+		        s = '',
+		        toFixedFix = function (n, prec) {
+		            var k = Math.pow(10, prec);
+		            return '' + Math.round(n * k) / k;
+		        };
+		        
+	    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+	    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+	    if (s[0].length > 3) {
+	        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+	    }
+	    if ((s[1] || '').length < prec) {
+	        s[1] = s[1] || '';
+	        s[1] += new Array(prec - s[1].length + 1).join('0');
+	    }
+	    return s.join(dec);
 	
+	};
+
 	self.init();
 };
-
-
-
-$(document).ready(function(){
-	var thermometer = new Thermometer($(".brown-bag"), $(".pink-bag"), $(".pink-bag-top"), 'images/pink_bag.png');
-});
